b98e09c61ff5df6ef7974fdab5f1fa5487608bed
PaulProduktion <julien7495@gmail.com>
2022-01-26 14:21:22 +0100

    changement

---
+++ Restaurant/main.js
@@ -0,0 +1,24 @@
+/*var marc = {
+  nom: "marc",
+  age: 14,
+  moyenne: 15,
+};
+*/
+// console.log(marc.age >= 18 ? "tu es majeur" : "tu es mineur");
+
+/* Vous pouvez changer le contenu de ces variables */
+let a = 2
+let b = 2
+let c = 2
+
+if (a === b && b===c  ) {
+  console.log("Les trois variables sont identiques.")
+} else if (b===c ||a===c || a===b ) {
+  console.log("Les deux variables sont identiques.")
+} else {
+  console.log("Les trois sont différentes.")
+}
+
+function name(params) {
+  
+}
